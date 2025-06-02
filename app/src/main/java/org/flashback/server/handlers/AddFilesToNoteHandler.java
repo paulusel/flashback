f565919c1b61ce90e226383d0ab82eb485521f33
paulusel <paulusel@proton.me>
2025-06-02 08:06:52 +0300

    accept newly added files in a multipart/multiform upload

--- app/src/main/java/org/flashback/server/handlers/AddFilesToNoteHandler.java
+++ app/src/main/java/org/flashback/server/handlers/AddFilesToNoteHandler.java
@@ -4,10 +4,10 @@ import org.eclipse.jetty.http.HttpStatus;
 import org.flashback.auth.Authenticator;
 import org.flashback.database.Database;
 import org.flashback.exceptions.FlashbackException;
-import org.flashback.types.FileAddRemoveRequest;
-import org.flashback.types.MessageResponse;
+import org.flashback.types.Note;
+import org.flashback.types.NoteResponse;
 import org.flashback.types.RequestResponsePair;
-import org.flashback.helpers.Json;
+import org.flashback.helpers.NoteProcessor;
 import org.flashback.helpers.GenericHandler;
 
 public class AddFilesToNoteHandler {
@@ -15,12 +15,18 @@ public class AddFilesToNoteHandler {
     public static void handle(RequestResponsePair exchange) {
         try {
             Long userId = Authenticator.authenticate(exchange.getRequest());
-            GenericHandler.checkJsonBody(exchange.getRequest());
-            String json = GenericHandler.requestBodyString(exchange.getRequest());
-            FileAddRemoveRequest[] requests = Json.deserialize(json, FileAddRemoveRequest[].class);
-            Database.addNoteFiles(userId, requests);
-            MessageResponse response = new MessageResponse(true, HttpStatus.OK_200, "added successfully");
-            GenericHandler.sendResponse(response, exchange);
+            Note note = null;
+            try {
+                note = NoteProcessor.extractNoteFromForm(exchange.getRequest());
+                Database.updateNote(userId, note);
+                NoteProcessor.postProcessFiles(userId, note);
+            }
+            catch(Exception e) {
+                if(note != null) {
+                    NoteProcessor.cleanFiles(userId, note);
+                }
+            }
+            GenericHandler.sendResponse(new NoteResponse(true, HttpStatus.CREATED_201, note), exchange);
         }
         catch(FlashbackException e) {
             GenericHandler.handleException(exchange, e);
